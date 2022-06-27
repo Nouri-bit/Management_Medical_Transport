8eccf767c3de3bc8b7223869316cb5f4efbce08a
Boutheyna NOURI <62301227+Nouri-bit@users.noreply.github.com>
2022-06-27 15:52:10 +0000

    Merge branch 'master' of https://github.com/Nouri-bit/2cs_18.git

--- src/main/java/net/dao/Chauffeurs.java
+++ src/main/java/net/dao/Chauffeurs.java
@@ -39,7 +39,7 @@ public class Chauffeurs {
 				A.setType(t.getInt("type"));
 				A.setIdoperateur(t.getInt("idoperateur"));
 				System.out.println(A.getNss());
-				String sql2= "select dt, jr, kml, att, ty , tr from (select s.date dt ,s.jour jr,s.kmreel kml, s.attente att, s.type ty, s.tranche tr  from \r\n"
+				String sql2= "select dt, jr, kml, att, ty , tr, idd from (select s.date dt , s.jour jr, s.idseance idd, s.kmreel kml, s.attente att, s.type ty, s.tranche tr  from \r\n"
 						+ "public.\"chauffeur \" ch , public.seance s  where ch.nss=? and s.idchauffeur = ch.nss and s.kmreel>0 and s.etat='valide' ) resultat ; "; 
 				PreparedStatement statement2=connection.prepareStatement(sql2);
 				statement2.setLong(1,A.getNss());
@@ -48,38 +48,126 @@ public class Chauffeurs {
 					
 					Seance seance = new Seance();
 					seance.setJour(t2.getString("jr"));
+					seance.setIdseance(t2.getInt("idd"));
 					seance.setKmreel(t2.getFloat("kml"));
 					seance.setAttente(t2.getBoolean("att"));
 					seance.setType(t2.getInt("tr"));
 					seance.setTranche(t2.getInt("ty"));
 					SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
 					seance.setDate(simpleDateFormat.parse(t2.getString("dt")));
-					double montant =0;
-					if(A.getType()==1) {
+					
+String squl="SELECT tr, compte,j FROM (SELECT ch.nss numero, count(*) compte, s.tranche tr, s.jour j, s.type ty \r\n"
+		+ "													FROM public.\"chauffeur \" ch, public.\"seance\" s , public.\"patient\" p\r\n"
+		+ "													where   s.date=? and s.tranche=? and ch.nss=? and ch.nss= s.idchauffeur and p.nss= s.idpatient \r\n"
+		+ "							                             \r\n"
+		+ "														group by s.jour , s.tranche , ch.nss, s.type\r\n"
+		+ "														\r\n"
+		+ "														order by count(*) , numero)\r\n"
+		+ "														resultat\r\n"
+		+ "														;";
+PreparedStatement statement22=connection.prepareStatement(squl);
+	statement22.setString(1, ""+seance.getDate());
+	statement22.setInt(2, seance.getTranche());
+	statement22.setLong(3, A.getNss());
+	ResultSet t22=statement22.executeQuery();
+	int nombre= t22.getInt("compte");
+    double dis=0;
+    double montant=0;
+                if((t22.getString("j").equals("samedi")) || (t22.getString("j").equals("vendredi")) || (t22.getInt("tr")==3)) {
+                	if(A.getType()==1) {
 						if(seance.isAttente()==true) {
+							//MAZAAL
 							
 						}
 						else {
-							
+							if(seance.getKmreel()>100) {
+							dis= seance.getKmreel()-100;
+							montant=((100*27)+(dis*19))*1.25;
+							}
+							else {
+								montant=(27*(seance.getKmreel()))*1.25;
+							}
 						}
 					}
 					else if (A.getType()==2) {
 						if(seance.isAttente()==true) {
-							
+							//MAZAAL
 						}
 						else {
+							if(seance.getKmreel()>100) {
+								dis= seance.getKmreel()-100;
+								montant=((100*18)+(dis*13.5))*1.25;
+								}
+								else {
+									montant=(18*(seance.getKmreel()))*1.25;
+								}
+							}
 							
-						}
+						
 					}
 					else {
 						if(seance.isAttente()==true) {
-							
+							//MAZAAL
 						}
 						else {
-							
+							if(seance.getKmreel()>100) {
+							dis= seance.getKmreel()-100;
+							montant=((100*12)+(dis*9));
+							}
+							else {
+								montant=(12*(seance.getKmreel()));
+							}
 						}
 						
 					}
+                }
+                else {
+					if(A.getType()==1) {
+						if(seance.isAttente()==true) {
+							//MAZAAL
+							
+						}
+						else {
+							if(seance.getKmreel()>100) {
+							dis= seance.getKmreel()-100;
+							montant=(100*27)+(dis*19);
+							}
+							else {
+								montant=27*(seance.getKmreel());
+							}
+						}
+					}
+					else if (A.getType()==2) {
+						if(seance.isAttente()==true) {
+							//MAZAAL
+						}
+						else {
+							if(seance.getKmreel()>100) {
+								dis= seance.getKmreel()-100;
+								montant=(100*18)+(dis*13.5);
+								}
+								else {
+									montant=18*(seance.getKmreel());
+								}
+							}
+							
+						
+					}
+					else {
+						if(seance.isAttente()==true) {
+							//MAZAAL
+						}
+						else {
+							if(seance.getKmreel()>100) {
+							dis= seance.getKmreel()-100;
+							montant=((100*12)+(dis*9));
+							}
+							else {
+								montant=(12*(seance.getKmreel()));
+							}
+						}
+						
+					}}
 					seance.setMontant(montant);
 					ss.add(seance);
 				}
--- src/main/java/net/dao/ValidationSeance.java
+++ src/main/java/net/dao/ValidationSeance.java
@@ -167,6 +167,8 @@ public int ajoutkilometrage(Seance A) throws ClassNotFoundException{
 	 
 	return tab; 
 }
+//////////////////////////////////////////////////////////////
+////////////////////////VALIDER //////////////////////////////
 public int valider(Seance A) throws ClassNotFoundException{
 	 int tab = 0; 
 	Class.forName("org.postgresql.Driver")	;
@@ -188,10 +190,55 @@ public int valider(Seance A) throws ClassNotFoundException{
 			ResultSet rs1 =statement.executeQuery();
 			ResultSet rs11 =statement22.executeQuery();
 			if(rs1.next() && rs11.next()) {
+				
+	                	   String sqll = "UPDATE public.seance\r\n"
+	               				+ "	SET etat='valide' \r\n"
+	               				+ "	 where idseance=?;";
+	                	   PreparedStatement statemen=connection.prepareStatement(sqll);
+	               		statemen.setFloat(1,A.getIdseance());
+	               		statemen.executeUpdate();
+	               		
+	               	    System.out.println(statemen); 
+	                	   
+	                   
+	                   
+			}
+			
+	 }catch(Exception e){
+			e.printStackTrace();
+		};
+	 
+	return tab; 
+}
+//////// DONNER DISTANCE /////////////////////////////////
+public double donner_distance(Seance A) throws ClassNotFoundException{
+	 double distance = 0; 
+	Class.forName("org.postgresql.Driver")	;
+	try {
+		Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5433/2cs_project_18","postgres", "20001999");
+		
+		
+		 String sql= "SELECT  o.idoperateur, addresse\r\n"
+		 		+ "	FROM public.\"chauffeur \" ch, public.\"operateur\" o\r\n"
+		 		+ "    where ch.nss=? and ch.idoperateur= o.idoperateur \r\n"
+		 		+ "    ;";
+		 String sql2="    SELECT  addresse, addresseh\r\n"
+			 		+ "        FROM public.\"patient\"\r\n"
+			 		+ "        where nss=? \r\n"
+			 		+ "        ;";
+			PreparedStatement statement=connection.prepareStatement(sql);
+			PreparedStatement statement22=connection.prepareStatement(sql2);
+			statement22.setLong(1,A.getIdpatient());
+			statement.setLong(1,A.getIdchauffeur());
+			ResultSet rs1 =statement.executeQuery();
+			ResultSet rs11 =statement22.executeQuery();
+			if(rs1.next() && rs11.next()) {
 				String addrch= rs1.getString("addresse").replace(' ', '%');
 				String addrpa= rs11.getString("addresse").replace(' ', '%');
+				String addrpah= rs11.getString("addresseh").replace(' ', '%');
 				System.out.println(rs1.getString("addresse"));
 				System.out.println(rs11.getString("addresse"));
+				System.out.println(rs11.getString("addresseh"));
 				///////////////// @ chauffeur
 	                    URL url = new URL("http://www.mapquestapi.com/geocoding/v1/address?key=5rbG94EZ1CBfSDepJwvayTwiYOG5Fgm9&location="+addrch);
                          System.out.println(url);
@@ -251,121 +298,38 @@ public int valider(Seance A) throws ClassNotFoundException{
 	                            JSONObject e3 = (JSONObject) d3.get("displayLatLng");
 	                            System.out.println(e3.get("lng"));
 	                            System.out.println(e3.get("lat"));
+	                    ////////////////////////////////// strcuture sanitaire ////////////////////////////////
+	                            URL url33 = new URL("http://www.mapquestapi.com/geocoding/v1/address?key=5rbG94EZ1CBfSDepJwvayTwiYOG5Fgm9&location="+addrpah);
+
+		                        HttpURLConnection conn33 = (HttpURLConnection) url33.openConnection();
+		                        conn33.setRequestMethod("GET");
+		                        conn33.connect();
+		                            StringBuilder informationString33 = new StringBuilder();
+		                            Scanner scanner33 = new Scanner(url33.openStream());
+
+		                            while (scanner33.hasNext()) {
+		                                informationString33.append(scanner33.nextLine());
+		                            }
+		                            //Close the scanner  33
+		                            scanner33.close();
+		                            JSONParser parse33 = new JSONParser();
+		                            JSONObject dataObject33 = (JSONObject) parse33.parse(String.valueOf(informationString33));
+		                            JSONObject c33= (JSONObject) ((ArrayList) dataObject33.get("results")).toArray()[0];
+		                            JSONObject  d33= (JSONObject) ((ArrayList) c33.get("locations")).toArray()[0];
+		                            JSONObject e33 = (JSONObject) d33.get("displayLatLng");
+		                            System.out.println(e33.get("lng"));
+		                            System.out.println(e33.get("lat"));
 	                    ///////////////////////////////calcul de la distance //////////////////////////////////
 	                            double pi = 3.1415/180;
-	                            double distance = 6371*Math.acos(((Math.sin((Double) e3.get("lat")*pi))*(Math.sin((Double) e.get("lat")*pi)))+ ((Math.cos(((Double) e3.get("lat"))*pi))*(Math.cos(((Double) e.get("lat"))*pi))*(Math.cos(pi*(((Double) e3.get("lng"))-((Double) e.get("lng"))))))) ;
+	                            double dis1 =  6371*Math.acos(((Math.sin((Double) e3.get("lat")*pi))*(Math.sin((Double) e33.get("lat")*pi)))+ ((Math.cos(((Double) e3.get("lat"))*pi))*(Math.cos(((Double) e33.get("lat"))*pi))*(Math.cos(pi*(((Double) e3.get("lng"))-((Double) e33.get("lng")))))));
+	                            double distance1 = 6371*Math.acos(((Math.sin((Double) e3.get("lat")*pi))*(Math.sin((Double) e.get("lat")*pi)))+ ((Math.cos(((Double) e3.get("lat"))*pi))*(Math.cos(((Double) e.get("lat"))*pi))*(Math.cos(pi*(((Double) e3.get("lng"))-((Double) e.get("lng"))))))) ;
+	                           distance = dis1+distance1;
+	                           System.out.println(dis1);
+	                           System.out.println(distance1);
 	                            System.out.println(distance);
 	                            ///////////////////////////////////////////////////////////
-	                 
-	                	   String sqll = "UPDATE public.seance\r\n"
-	               				+ "	SET etat='valide' \r\n"
-	               				+ "	 where idseance=?;";
-	                	   PreparedStatement statemen=connection.prepareStatement(sqll);
-	               		statemen.setFloat(1,A.getIdseance());
-	               		statemen.executeUpdate();
-	               		
-	               	    System.out.println(statemen); 
+	                  
 	                	   
-	                   
-	                   
-			}
-			
-	 }catch(Exception e){
-			e.printStackTrace();
-		};
-	 
-	return tab; 
-}
-public double donner_distance(Seance A) throws ClassNotFoundException{
-	 double distance = 0; 
-	Class.forName("org.postgresql.Driver")	;
-	try {
-		Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5433/2cs_project_18","postgres", "20001999");
-		
-		
-		 String sql= "SELECT  o.idoperateur, addresse\r\n"
-		 		+ "	FROM public.\"chauffeur \" ch, public.\"operateur\" o\r\n"
-		 		+ "    where ch.nss=? and ch.idoperateur= o.idoperateur \r\n"
-		 		+ "    ;";
-		 String sql2="    SELECT  addresse\r\n"
-		 		+ "        FROM public.\"patient\"\r\n"
-		 		+ "        where nss=? \r\n"
-		 		+ "        ;";
-			PreparedStatement statement=connection.prepareStatement(sql);
-			PreparedStatement statement22=connection.prepareStatement(sql2);
-			statement22.setLong(1,A.getIdpatient());
-			statement.setLong(1,A.getIdchauffeur());
-			ResultSet rs1 =statement.executeQuery();
-			ResultSet rs11 =statement22.executeQuery();
-			if(rs1.next() && rs11.next()) {
-				String addrch= rs1.getString("addresse").replace(' ', '%');
-				String addrpa= rs11.getString("addresse").replace(' ', '%');
-				System.out.println(rs1.getString("addresse"));
-				System.out.println(rs11.getString("addresse"));
-				///////////////// @ chauffeur
-	                    URL url = new URL("http://www.mapquestapi.com/geocoding/v1/address?key=5rbG94EZ1CBfSDepJwvayTwiYOG5Fgm9&location="+addrch);
-                        System.out.println(url);
-	                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	                    conn.setRequestMethod("GET");
-	                    conn.connect();
-
-	                    //Check if connect is made
-	                    int responseCode = conn.getResponseCode();
-
-	                    // 200 OK
-	                  /*  if (responseCode != 200) {
-	                        throw new RuntimeException("HttpResponseCode: " + responseCode);
-	                    } else {*/
-
-	                        StringBuilder informationString = new StringBuilder();
-	                        Scanner scanner = new Scanner(url.openStream());
-
-	                        while (scanner.hasNext()) {
-	                            informationString.append(scanner.nextLine());
-	                        }
-	                        //Close the scanner
-	                        scanner.close();
-
-	                        //System.out.println(informationString);
-
-
-	                        //JSON simple library Setup with Maven is used to convert strings to JSON
-	                        JSONParser parse = new JSONParser();
-	                        JSONObject dataObject = (JSONObject) parse.parse(String.valueOf(informationString));
-	                        //JSONObject site = (JSONObject)(((JSONArray)dataObject.get(0)).get(0));
-	                        //Get the first JSON object in the JSON array
-	                        //((ArrayList) dataObject.get("results")).toArray();
-	                        JSONObject c= (JSONObject) ((ArrayList) dataObject.get("results")).toArray()[0];
-	                        JSONObject  d= (JSONObject) ((ArrayList) c.get("locations")).toArray()[0];
-	                        JSONObject e = (JSONObject) d.get("displayLatLng");
-	                        System.out.println(e.get("lng"));
-	                        System.out.println(e.get("lat"));
-	                        //////// @ patient 
-	                        URL url3 = new URL("http://www.mapquestapi.com/geocoding/v1/address?key=5rbG94EZ1CBfSDepJwvayTwiYOG5Fgm9&location="+addrpa);
-
-	                        HttpURLConnection conn3 = (HttpURLConnection) url3.openConnection();
-	                        conn3.setRequestMethod("GET");
-	                        conn3.connect();
-	                            StringBuilder informationString3 = new StringBuilder();
-	                            Scanner scanner3 = new Scanner(url3.openStream());
-
-	                            while (scanner3.hasNext()) {
-	                                informationString3.append(scanner3.nextLine());
-	                            }
-	                            //Close the scanner
-	                            scanner3.close();
-	                            JSONParser parse3 = new JSONParser();
-	                            JSONObject dataObject3 = (JSONObject) parse3.parse(String.valueOf(informationString3));
-	                            JSONObject c3= (JSONObject) ((ArrayList) dataObject3.get("results")).toArray()[0];
-	                            JSONObject  d3= (JSONObject) ((ArrayList) c3.get("locations")).toArray()[0];
-	                            JSONObject e3 = (JSONObject) d3.get("displayLatLng");
-	                            System.out.println(e3.get("lng"));
-	                            System.out.println(e3.get("lat"));
-	                    ///////////////////////////////calcul de la distance //////////////////////////////////
-	                            double pi = 3.1415/180;
-	                            distance = 6371*Math.acos(((Math.sin((Double) e3.get("lat")*pi))*(Math.sin((Double) e.get("lat")*pi)))+ ((Math.cos(((Double) e3.get("lat"))*pi))*(Math.cos(((Double) e.get("lat"))*pi))*(Math.cos(pi*(((Double) e3.get("lng"))-((Double) e.get("lng"))))))) ;
-	                            System.out.println(distance);
-	                            ///////////////////////////////////////////////////////////
 			}
 			
 	 }catch(Exception e){
